3de10870856201d78d9c51476b0f90a6cba74f7e
Sri <srivalli.sivaramasankaran@freshworks.com>
2025-10-27 18:03:30 +0530

    fix(icon): add a fallback version for icon path

--- packages/crayons-core/src/components/icon/library.crayons.ts
+++ packages/crayons-core/src/components/icon/library.crayons.ts
@@ -1,7 +1,8 @@
 import { __CRAYONS_ICON_VERSION__ } from '@freshworks/crayons-icon';
 import type { FwIconLibrary } from './library.icon.utils';
 
-const CRAYONS_ICONS_ASSET_PATH = `https://cdn.jsdelivr.net/npm/@freshworks/crayons-icon@${__CRAYONS_ICON_VERSION__}/dist/icons`;
+const version = __CRAYONS_ICON_VERSION__ || 'next';
+const CRAYONS_ICONS_ASSET_PATH = `https://cdn.jsdelivr.net/npm/@freshworks/crayons-icon@${version}/dist/icons`;
 const library: FwIconLibrary = {
   name: 'crayons',
   resolver: (name) => `${CRAYONS_ICONS_ASSET_PATH}/${name}.svg`,
