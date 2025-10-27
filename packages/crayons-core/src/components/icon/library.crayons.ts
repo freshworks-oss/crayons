c9998cbdffecdb446b8b4b7c93461e19c62ef540
Sri <srivalli.sivaramasankaran@freshworks.com>
2025-10-27 17:35:05 +0530

    fix(icon): replace static icon path with pkg version

--- packages/crayons-core/src/components/icon/library.crayons.ts
+++ packages/crayons-core/src/components/icon/library.crayons.ts
@@ -1,6 +1,7 @@
+import { __CRAYONS_ICON_VERSION__ } from '@freshworks/crayons-icon';
 import type { FwIconLibrary } from './library.icon.utils';
-const CRAYONS_ICONS_ASSET_PATH =
-  'https://cdn.jsdelivr.net/npm/@freshworks/crayons-icon@next/dist/icons';
+
+const CRAYONS_ICONS_ASSET_PATH = `https://cdn.jsdelivr.net/npm/@freshworks/crayons-icon@${__CRAYONS_ICON_VERSION__}/dist/icons`;
 const library: FwIconLibrary = {
   name: 'crayons',
   resolver: (name) => `${CRAYONS_ICONS_ASSET_PATH}/${name}.svg`,
--- packages/crayons-icon/gulpfile.js
+++ packages/crayons-icon/gulpfile.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
+const pkg = require('./package.json');
 
 const generateIconsExportData = async () => {
   const icons_out_dir = './dist/icons';
@@ -27,7 +28,7 @@ const generateIconsExportData = async () => {
     }
   };
   try {
-    let indexData = '';
+    let indexData = `export const __CRAYONS_ICON_VERSION__ = "${pkg.version}";\n`;
     const allSvgFiles = await fs.readdir(path.join(icons_out_dir, ''));
     for (const svgFile of allSvgFiles) {
       indexData = indexData + (await getIconsSVGData(svgFile)) + '\n';
